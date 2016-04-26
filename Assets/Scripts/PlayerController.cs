afcdf457d59f597ae8240d6b83935cfc0395493b
Solobolt <solobolt@gmail.com>
2016-04-26 17:47:57 +1000

    Added Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Renderer rend;
+
+    [Range(0.0f,1.0f)]
+    public float fadeAmount = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rend.material.color = Color.Lerp(Color.red,Color.green,fadeAmount);
+	}
+}
