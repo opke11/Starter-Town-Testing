64e160024d68ea0aa9a75d87cc8ac8c886248c1d
Solobolt <solobolt@gmail.com>
2016-04-26 19:42:22 +1000

    Started UI Manager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+    //Holds Paused screen Objects 
+    public GameObject pausedScreen;
+    private GameObject currentPause;
+    private bool isPaused = false;
+
+    //Holds Collectables Objects
+    private bool showCollectables = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(Input.GetKeyDown("escape"))
+        {
+            TogglePause();
+        }
+	}
+
+    //Exits the game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    //Starts the game
+    public void StartGame()
+    {
+        Application.LoadLevel(0);
+    }
+
+    // Swaps the paused bool
+    public void TogglePause()
+    {
+        if(isPaused == true)
+        {
+            isPaused = false;
+            Time.timeScale = 1;
+
+            //Hide UI
+            Destroy(currentPause.gameObject);
+        }
+        else
+        {
+            isPaused = true;
+            Time.timeScale = 0;
+
+            //Show UI
+            currentPause = Instantiate(pausedScreen) as GameObject;
+        }
+
+    }
+}
